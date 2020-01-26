90b6e2d0abf5bc87e4688cef22a4947007e6a2f2
Neto Deolino <netodeolino@gmail.com>
2020-01-26 16:37:03 -0300

    share flutter

--- pdc_flutter/lib/src/ui/home.dart
+++ pdc_flutter/lib/src/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_share_me/flutter_share_me.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -20,16 +21,51 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Prova de Conceito'),
       ),
-      body: Center(
+      body: Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Container(
+              height: 40,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color(0xFFF58524),
+                    Color(0XFFF92B7F),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Text(
+                    "Login",
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () {
+                    FlutterShareMe().shareToTwitter(
+                       url: 'https://github.com/netodeolino', msg: 'teste');
+                  }
+                ),
+              ),
             ),
           ],
         ),
